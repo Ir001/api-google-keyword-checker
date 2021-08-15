a1ff6f1936aefd2a3b452553718ffc0f3a12c4c3
ir001 <irwanantonio2708@gmail.com>
2021-08-15 23:42:03 +0700

    port

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const main = require('./main')
+const app = express()
+const port = process.env.PORT || 3000;
+app.get('/', async (req, res) => {
+    const keyword = req.query?.keyword;
+    if(keyword == undefined || keyword == '' || keyword == null){
+        return res.json({success:false,message:'Keyword required'});
+    }
+    try{
+        const response = await main.request(keyword);
+        return res.json(response);
+    }catch(err){
+        return res.json({success:false,message: err.toString()});
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
---
+++ main.js
@@ -0,0 +1,46 @@
+const axios = require('axios').default;
+const cheerio = require('cheerio');
+const userAgent = require('user-agents');
+main = {
+    request : async (keyword)=>{
+        const ua = new userAgent({deviceCategory:'desktop'}).toString();
+        const {data} = await axios.get(`http://www.google.co.id/search?q=${main.formatKeyword(keyword)}`,{
+            headers : {
+                'User-Agent' : ua
+            }
+        });
+        const $ = cheerio.load(data);
+        const result = $('body').find('div[id=result-stats]')?.text()?.trim();
+        const response = main.format(result);
+        return {
+            success : true,
+            data : {
+                keyword,
+                ...response
+            }
+        };
+    },
+    formatKeyword : (value)=>{
+        const regex = /\s/g;
+        return value?.replace(regex,'+');
+    },
+    format : (value)=>{
+        const regexs = [
+            /Sekitar (.*) hasil \((.*?) detik\)/gm,
+            /About (.*) results \((.*?) seconds\)/gm
+        ];
+        let response;
+        regexs.map((regex,index)=>{
+            const result = regex.exec(value);
+            if(result?.[1] != undefined && result?.[2] != undefined){
+                response =  {
+                    'result' : parseFloat(result[1]?.replace(/\./g,'')),
+                    'time' : parseFloat(result[2]?.replace(/\,/g,'.')),
+                }
+            }
+        })
+        return response;
+    },
+};
+
+module.exports = main;
