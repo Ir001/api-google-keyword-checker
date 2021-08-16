33aabbab6deca6497d3849f8681b290561cd5e06
ir001 <irwanantonio2708@gmail.com>
2021-08-16 21:59:32 +0700

    fix(request) raw request body

--- index.js
+++ index.js
@@ -20,9 +20,8 @@ app.post('/api/', async (req, res, next) => {
     try {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-        const raw = req.rawBody?.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ');
-        const rawBody = JSON.parse(raw);
+        res.header('Access-Control-Allow-Methods', 'POST');
+        const rawBody = JSON.parse(req.rawBody);
         const data = rawBody?.keywords.map(async (value,index)=>{
             if(value != null && value != undefined && value != ''){
                 return await main.request(value);
